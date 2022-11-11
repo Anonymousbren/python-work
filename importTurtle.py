62ca9895fc146c9329f5282e2988abbe26b6eeac
anonymous <anonymousbren@protonmail.com>
2022-11-11 02:25:04 -0800

    this is nice

--- importTurtle.py
+++ importTurtle.py
@@ -1,3 +1,4 @@
+from asyncio import run
 import turtle
 t = turtle.Turtle()
 
@@ -7,7 +8,7 @@ screen.bgcolor('black')
 #creating a rectangle
 t.color('white')
 t.shape('arrow')
-t.speed())
+t.speed()
 t.fillcolor('blue')
 t.begin_fill()
 t.forward(200)
@@ -18,6 +19,9 @@ t.left(90)
 t.forward(200)
 t.end_fill()
 turtle.done()
+
+if __name__ == '__main__':
+         run()
 
 
 
